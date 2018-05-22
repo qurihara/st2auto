468a39fdafabbdad7b3589ba113eb1b6704afe08
qurihara <qurihara@unryu.org>
2018-05-23 08:50:05 +0900

    ok

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //var machina = require('machina');
 var machina = require('./node_modules/machina/lib/machina.js')
-var isCanceled = false;
+// var isCanceled = false;
+var ryu;
 
 var Ryu = machina.Fsm.extend({
   initialState: 'N',
@@ -8,23 +9,27 @@ var Ryu = machina.Fsm.extend({
     'N': {  // 各状態
       fight: async function() { // その状態が扱う振る舞い（関数）
         console.log('neutral');
-        await send("hello");
-        await wait(2000);
-        await send(" world ");
-        await wait(2000);
+        try{
+          await send('N', "hello");
+          await wait(2000);
+          await send('N', " world ");
+          await wait(2000);
+        }catch (err) {
+          console.log(err)
+        }
       }
     },
     'Hado': {
       fight: async function() {
         console.log('Hado');
-        await send("Hadoken");
+        await send('Hado', "Hadoken");
         await wait(2000);
       }
     },
     'Shoryu': {
       fight: async function() {
         console.log('Shoryu');
-        await send("Shoryuken");
+        await send('Shoryu', "Shoryuken");
         await wait(2000);;
       }
     },
@@ -38,10 +43,10 @@ var Ryu = machina.Fsm.extend({
 });
 
 async function main(){
-  var ryu = new Ryu();
+  ryu = new Ryu();
   ryu.fight();
-  await wait(2000);
-  isCanceled = false;
+  await wait(1000);
+  // await setIsCanceled(true);
   await ryu.shift('Hado');
   await ryu.fight();
   await wait(2000);
@@ -50,13 +55,21 @@ async function main(){
 }
 main();
 
-function send(GCDSL){
+// function setIsCanceled(_isCanceled){
+//   return new Promise(function(resolve, reject) {
+//       isCanceled = _isCanceled;
+//       resolve();
+//   });
+// }
+
+function send(ifStateIs, GCDSL){
   return new Promise(function(resolve, reject) {
-    if (isCanceled === false){
+    // if (isCanceled === false){
+    if (ifStateIs === ryu.state){
       console.log(GCDSL);
       resolve();
     }else{
-      reject();
+      reject("canceled.");
     }
     // SEND(function(err,data){
     //   if (!err){
@@ -68,39 +81,9 @@ function send(GCDSL){
   });
 }
 
-function wait_with_flag(delay,isCanceled) {
-    return new Promise(function(resolve, reject) {
-        if(isCanceled === true){
-            reject();
-        }else{
-          setTimeout(resolve, delay);
-        }
-    });
-}
-
-// function wait_with_flag(delay,isCanceled) {
-//   return new Promise(function(resolve, reject) {
-//       setTimeout(
-//         function(resolve, reject,isCanceled) {
-//           if(isCanceled === true){
-//             reject();
-//           }else{
-//             resolve();
-//           }
-//         }
-//         , delay);
-//   });
-// }
-
-
 // const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 function wait(delay) {
     return new Promise(function(resolve, reject) {
         setTimeout(resolve, delay);
     });
 }
-// // 使い方
-// wait(1000)
-//     .then(function() {
-//         // 処理
-//     });
